43859b0d9cd13d774273aede90c566146945c4bd
billiejoenahema <billiejoenahema@gmail.com>
2021-12-04 09:46:27 +0900

    create: photo detail component

--- web/resources/js/router/index.js
+++ web/resources/js/router/index.js
@@ -2,11 +2,17 @@ import { createRouter, createWebHistory } from 'vue-router';
 import PhotoList from '../pages/PhotoList';
 import UserLogin from '../pages/UserLogin';
 import SystemError from '../pages/errors/SystemError.vue';
+import PhotoDetail from '../components/PhotoDetail.vue';
 
 const routes = [
   {
     path: '/',
     component: PhotoList,
+  },
+  {
+    path: '/photos/:id',
+    component: PhotoDetail,
+    props: true,
   },
   {
     path: '/login',
