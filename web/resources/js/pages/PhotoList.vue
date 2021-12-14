3aeb191f28f4250c46905c0aa22e80fbede06834
billiejoenahema <billiejoenahema@gmail.com>
2021-12-15 08:06:25 +0900

    fix: photo list

--- web/resources/js/pages/PhotoList.vue
+++ web/resources/js/pages/PhotoList.vue
@@ -1,9 +1,28 @@
-<template>
-  <h1>Photo List</h1>
-</template>
+<script setup>
+import { computed, onMounted } from 'vue';
+import Photo from '../components/Photo.vue';
+import { useStore } from 'vuex';
 
-<script>
-export default {
-  setup() {},
-};
+const store = useStore();
+
+store.dispatch('photo/getPhotos');
+const photos = computed(
+  () => store.getters['photo/photos']
+);
+onMounted(() => {
+  console.log(photos.value);
+});
 </script>
+
+<template>
+  <div class="photo-list">
+    <div class="grid">
+      <Photo
+        class="grid__item"
+        v-for="photo in photos"
+        :key="photo.id"
+        :item="photo"
+      />
+    </div>
+  </div>
+</template>
