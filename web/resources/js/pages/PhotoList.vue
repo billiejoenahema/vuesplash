ed4aed4877b8cc0d6d191aa1361940ef1d64ec83
billiejoenahema <billiejoenahema@gmail.com>
2021-12-22 08:10:51 +0900

    remove: unnecessary code

--- web/resources/js/pages/PhotoList.vue
+++ web/resources/js/pages/PhotoList.vue
@@ -1,10 +1,5 @@
 <script setup>
-import {
-  computed,
-  defineProps,
-  ref,
-  watchEffect,
-} from 'vue';
+import { computed, defineProps, watchEffect } from 'vue';
 import { useStore } from 'vuex';
 import Photo from '../components/Photo';
 import Pagination from '../components/Pagination';
@@ -17,22 +12,6 @@ const props = defineProps({
     default: 1,
   },
 });
-
-const searchWord = ref('');
-const search = () => {
-  const elements = Array.from(
-    document.getElementsByClassName('messageBody')
-  );
-  const regexp = new RegExp(
-    '(' + searchWord.value + ')',
-    'gi'
-  );
-  const highlight = '<span class="highlight">$1</span>';
-  elements.forEach((el) => {
-    console.log(el);
-    el.innerHTML = el.innerHTML.replace(regexp, highlight);
-  });
-};
 
 const currentPage = computed(
   () => store.getters['photo/currentPage']
@@ -51,31 +30,7 @@ watchEffect(() => {
 });
 </script>
 
-<style>
-.highlight {
-  color: red;
-}
-</style>
-
 <template>
-  <input v-model="searchWord" />
-  <button @click="search">検索</button>
-  <div class="messageBody">
-    この連載記事では、フロントエンドに Vue.js + Vue Router +
-    Vuex とサーバーサイドに Laravel を使用したシングルページ
-    Web
-    アプリケーションの開発方法を紹介します。実際に写真共有アプリを開発する手順を通して
-    SPA 開発のエッセンスを学ぶことができます。
-    今回のチュートリアルで扱うツールなどのバージョンは以下の通りです。
-  </div>
-  <div class="messageBody">
-    この連載記事では、フロントエンドに Vue.js + Vue Router +
-    Vuex とサーバーサイドに Laravel を使用したシングルページ
-    Web
-    アプリケーションの開発方法を紹介します。実際に写真共有アプリを開発する手順を通して
-    SPA 開発のエッセンスを学ぶことができます。
-    今回のチュートリアルで扱うツールなどのバージョンは以下の通りです。
-  </div>
   <div class="photo-list">
     <div class="grid">
       <Photo
