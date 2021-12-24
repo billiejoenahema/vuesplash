10cbdcc9ce45662f9f6130e23b616ca0e3a17e47
billiejoenahema <billiejoenahema@gmail.com>
2021-12-25 08:51:11 +0900

    remove: unnecessary code

--- web/resources/js/pages/PhotoList.vue
+++ web/resources/js/pages/PhotoList.vue
@@ -1,10 +1,5 @@
 <script setup>
-import {
-  computed,
-  defineProps,
-  reactive,
-  watchEffect,
-} from 'vue';
+import { computed, defineProps, watchEffect } from 'vue';
 import { useStore } from 'vuex';
 import Photo from '../components/Photo';
 import Pagination from '../components/Pagination';
@@ -33,38 +28,9 @@ const photos = computed(
 watchEffect(() => {
   store.dispatch('photo/getPhotos', props.page);
 });
-
-const dateList = reactive([
-  { date: '1月1日' },
-  { date: '1月2日' },
-  { date: '1月2日' },
-  { date: '1月3日' },
-  { date: '1月3日' },
-  { date: '1月4日' },
-]);
-
-const current = {
-  date: '',
-};
-// メッセージの日付表示を制御
-const showDate = (date) => {
-  if (current.date !== date) {
-    current.date = date;
-    return true;
-  } else {
-    return false;
-  }
-};
 </script>
 
 <template>
-  <div v-for="(message, index) in dateList" :key="index">
-    // メッセージの日付
-    <div v-show="showDate(message.date)">
-      {{ message.date }}
-    </div>
-    <div>message body</div>
-  </div>
   <div class="photo-list">
     <div class="grid">
       <Photo
